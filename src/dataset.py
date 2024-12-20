960ac7617a46de928a1f347e8502da431c258b54
nurijunsu <nurijunsu@gmail.com>
2024-12-20 14:26:01 +0900

    first commit

---
+++ src/dataset.py
@@ -0,0 +1,6 @@
+from datasets import load_dataset
+sst2 = load_dataset("glue", "sst2")
+qnli = load_dataset("glue", "qnli")
+qqp = load_dataset("glue", "qqp")
+cifar100 = load_dataset("cifar100")
+superb_ic = load_dataset("superb", "ic")
